ee52f332e7932429c90f580725e07fe7393f1d4e
atttoum8643f <145755332+atttoum8643f@users.noreply.github.com>
2023-10-19 02:11:43 +0200

    Update Météo projetc.py

--- Météo projetc.py
+++ Météo projetc.py
@@ -9,7 +9,7 @@
 import requests
 
 def get_weather(city):
-    response = requests.get(f'http://api.weatherstack.com/current?access_key=c2591c1b872564a6299d9c0ed03475bd&query={city}')
+    response = requests.get(f'http://api.weatherstack.com/current?access_key=ccapi_keyd&query={city}')
     return response.json()
 
 # Utilisez la fonction pour obtenir la météo pour une ville spécifique
@@ -19,6 +19,37 @@ print(weather_data)
 
 # In[ ]:
 
+import requests
+import datetime
+
+def get_weather(api_key, location):
+    base_url = "http://api.weatherstack.com/current"
+    params = {
+        "access_key": api_key,
+        "query": location
+    }
+    response = requests.get(base_url, params=params)
+    data = response.json()
+    return data
+
+def display_weather(data):
+    current = data['current']
+    location = data['location']['name']
+    weather_icon = current['weather_icons'][0]
+    temperature = current['temperature']
+    localtime = data['location']['localtime']
+    weather_description = current['weather_descriptions'][0]
+
+    print(f"Location: {location}")
+    print(f"Weather Icon URL: {weather_icon}")
+    print(f"Temperature: {temperature}°C")
+    print(f"Local Time: {localtime}")
+    print(f"Weather Description: {weather_description}")
+
+api_key = "capi_key"
+location = "Marseille"
+data = get_weather(api_key, location)
+display_weather(data)
 
 
 
